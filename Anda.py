f7c975d53c2a04be9ee81c9c97bc733981eecd01
MartinChavez001 <martin.ezequiel.chave@gmail.com>
2025-10-19 00:37:14 -0300

    Anda v0.03 .Generacion de cartas numericas, faltas las cartas de tipo accion

--- Anda.py
+++ Anda.py
@@ -4,7 +4,7 @@ class Jugador():
         self.mazo_jugador = []
 
 class Carta():
-    def __ini__(self):
+    def __init__(self):
         self.total_cartas = 100
         self.numero = [1,2,3,4,5,6,7,8,9]
         self.colores = ["naranja", "violeta", "rosa", "marron"]
@@ -16,19 +16,29 @@ class Carta():
         self.cantidad_tomacuatro = 4
 
 class Mazo():
-    def __ini__(self, numero, colores, tipo,total_cartas,cantidad_tomados, cantidad_tomacuatro, cantidad_reserva,cantidad_salta):
+    def __init__(self, carta = None):
 
         self.cartas_generadas = {}
         self.mazo = {}
+        self.carta = carta or Carta()
     
-    def generacion_mazo (total_cartas, cartas_generadas, cantidad_numericas,cantidad_tomados, cantidad_tomacuatro, cantidad_reserva,cantidad_salta, mazo):
-        for i in total_cartas:
-            for numero, colores in cantidad_numericas:
-                if not cartas_generadas:
-                    cartas_generadas[len(cartas_generadas)+ 1] = (colores, numero, )
-                    print(cartas_generadas)
+    def generacion_mazo (self):
+        idx = 1
+        for color in self.carta.colores:
+            for numero in self.carta.numero:
+                for _ in range(2):
+                    self.cartas_generadas[idx] = (color, numero)
+                    idx += 1
+        
+        self.mazo = list(self.cartas_generadas.items())
+        print(f"Cartas generadas: {len(self.mazo)}")
+
+        for item in self.mazo[:self.carta.cantidad_numericas]:
+            print(item)
 
 
+m = Mazo()
+m.generacion_mazo()
 
 
 
